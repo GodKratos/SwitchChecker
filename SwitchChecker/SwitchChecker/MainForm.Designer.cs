e5c32a75d682dd9e8f44a2f08a543626fcc5789a
GodKratos_cp <SND\GodKratos_cp>
2010-04-01 01:07:21 +0000

    Add right click update option to switches

--- SwitchChecker/SwitchChecker/MainForm.Designer.cs
+++ SwitchChecker/SwitchChecker/MainForm.Designer.cs
@@ -31,7 +31,7 @@ namespace SwitchChecker
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Search Results");
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Search Results");
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -58,6 +58,7 @@ namespace SwitchChecker
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.panel2 = new System.Windows.Forms.Panel();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.updateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.switchMenuStrip.SuspendLayout();
@@ -217,10 +218,10 @@ namespace SwitchChecker
             this.treeView1.HideSelection = false;
             this.treeView1.Location = new System.Drawing.Point(0, 0);
             this.treeView1.Name = "treeView1";
-            treeNode3.Name = "Search Results";
-            treeNode3.Text = "Search Results";
+            treeNode1.Name = "Search Results";
+            treeNode1.Text = "Search Results";
             this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode3});
+            treeNode1});
             this.treeView1.ShowNodeToolTips = true;
             this.treeView1.ShowRootLines = false;
             this.treeView1.Size = new System.Drawing.Size(145, 572);
@@ -233,9 +234,10 @@ namespace SwitchChecker
             // 
             this.switchMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.editToolStripMenuItem,
+            this.updateToolStripMenuItem,
             this.deleteToolStripMenuItem});
             this.switchMenuStrip.Name = "switchMenuStrip";
-            this.switchMenuStrip.Size = new System.Drawing.Size(153, 70);
+            this.switchMenuStrip.Size = new System.Drawing.Size(153, 92);
             // 
             // editToolStripMenuItem
             // 
@@ -294,6 +296,13 @@ namespace SwitchChecker
             this.saveFileDialog1.Filter = "Excel Files|*.xls|All Files|*.*";
             this.saveFileDialog1.InitialDirectory = "C:\\Windows\\assembly\\GAC_MSIL\\System.Design\\2.0.0.0__b03f5f7f11d50a3a";
             // 
+            // updateToolStripMenuItem
+            // 
+            this.updateToolStripMenuItem.Name = "updateToolStripMenuItem";
+            this.updateToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.updateToolStripMenuItem.Text = "Update";
+            this.updateToolStripMenuItem.Click += new System.EventHandler(this.updateCurrentSwitchToolStripMenuItem_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -351,6 +360,7 @@ namespace SwitchChecker
         private ContextMenuStrip switchMenuStrip;
         private ToolStripMenuItem editToolStripMenuItem;
         private ToolStripMenuItem deleteToolStripMenuItem;
+        private ToolStripMenuItem updateToolStripMenuItem;
     }
 }
 
